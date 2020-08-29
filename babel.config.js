84c6151b3e1ac0c54b5778935b7de148c0670c53
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-08-29 00:08:45 -0500

    use babel-preset-atomic and cross-env

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,10 @@
-let presets = [];
+let presets = ["babel-preset-atomic"]
 
-let plugins = [
-  ["@babel/plugin-proposal-optional-chaining", { loose: false }],
-  ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
-
-];
-
-if (process.env.BABEL_ENV === "development") {
-  plugins.push(...[
-      "@babel/plugin-transform-modules-commonjs",
-      "@babel/plugin-proposal-export-namespace-from",
-  ]);
-}
+let plugins = []
 
 module.exports = {
   presets: presets,
   plugins: plugins,
   exclude: "node_modules/**",
   sourceMap: "inline",
-};
+}
